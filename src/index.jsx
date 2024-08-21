41c433092e04844aca6d3ebcb783ef382449c7e0
alessandrobattiato <alessandroDev@outlook.it>
2024-08-21 12:37:52 +0200

    feat: implement vanilla three.js solution to change background color

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,25 @@
-import './style.css'
-import ReactDOM from 'react-dom/client'
-import { Canvas } from '@react-three/fiber'
-import Experience from './Experience.jsx'
+import "./style.css";
+import ReactDOM from "react-dom/client";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience.jsx";
+import * as THREE from "three";
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
+const created = ({ scene }) => {
+    scene.background = new THREE.Color("red");
+};
 
 root.render(
     <Canvas
-        camera={ {
+        camera={{
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [ - 4, 3, 6 ]
-        } }
+            position: [-4, 3, 6],
+        }}
+        onCreated={created}
     >
         <Experience />
     </Canvas>
-)
+);
